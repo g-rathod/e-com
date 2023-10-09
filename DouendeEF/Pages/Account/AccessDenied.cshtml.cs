adb76b9d7ca0463faea8c0f1f0a9720f4c95e9f9
g-rathod <134310942+g-rathod@users.noreply.github.com>
2023-10-09 21:47:53 +0530

    Intial Commit

---
+++ DouendeEF/Pages/Account/AccessDenied.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DouendeEF.Pages.Account
+{
+    public class AccessDeniedModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
